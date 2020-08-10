8f86e51efd5ef92ff6946fc2606d501437cb34d4
kevin jiang <kevin.jiang@adelphi.digital>
2020-08-11 09:00:53 +1000

    fix up the image path issue

--- skinet/API/Startup.cs
+++ skinet/API/Startup.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using API.Extensions;
 using API.Helpers;
 using API.Middleware;
@@ -9,6 +10,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using StackExchange.Redis;
 
 namespace API
@@ -58,6 +60,14 @@ namespace API
       app.UseRouting();
 
       app.UseStaticFiles();
+
+      app.UseStaticFiles(new StaticFileOptions
+      {
+        FileProvider = new PhysicalFileProvider(
+          Path.Combine(Directory.GetCurrentDirectory(), "Content")
+        ),
+        RequestPath = "/content"
+      });
 
       app.UseAuthentication();
 
