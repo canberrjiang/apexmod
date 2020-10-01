e5a3f562f314aa83814f1b89d54ab7b2e65dbf06
kevin jiang <kevin.jiang@adelphi.digital>
2020-10-01 14:31:25 +1000

    fix delivery method dto data

--- skinet/API/Dtos/DeliveryMethodToCreateDto.cs
+++ skinet/API/Dtos/DeliveryMethodToCreateDto.cs
@@ -2,7 +2,7 @@ namespace API.Dtos
 {
   public class DeliveryMethodToCreate
   {
-    public string Name { get; set; }
+    public string ShortName { get; set; }
     public string Description { get; set; }
     public string DeliveryTime { get; set; }
     public decimal Price { get; set; }
