01e5d8e9eb4db0b2018249ce7c69328450bc7fd4
maxzhang686 <maxzhang686@gmail.com>
2020-10-01 20:04:09 +1000

    chang order list to order page

--- skinet/prototype/src/app/admin/admin-routing.module.ts
+++ skinet/prototype/src/app/admin/admin-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AdminComponent} from './admin.component';
 import { EditProductComponent } from './edit-product/edit-product.component';
+import { OrderListComponent } from "./order-list/order-list.component";
 import { OrderDetailAdminComponent } from "./order-detail-admin/order-detail-admin.component";
 
 const routes: Routes = [
   {path: '', component: AdminComponent},
   {path: 'create', component: EditProductComponent, data: {breadcrumb: 'Create'}},
   {path: 'edit/:id', component: EditProductComponent, data: {breadcrumb: 'Edit'}},
-  {path: 'adminorder/:id', component: OrderDetailAdminComponent, data: {breadcrumb: 'Order-Detail'}},
+  {path: 'adminorders', component: OrderListComponent, data: {breadcrumb: 'Order-list'}},
+  {path: 'adminorders/:id', component: OrderDetailAdminComponent, data: {breadcrumb: 'Order-Detail'}},
+  {path: 'deliveryinfo', component: OrderListComponent, data: {breadcrumb: 'Order-list'}},
 ];
 
 @NgModule({
